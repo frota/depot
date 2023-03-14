6a3ae710fdb35284a97cb5ab13b632e2516e1b02
Frota Silveira <frota.gm@gmail.com>
2023-03-14 13:24:07 -0300

    Task J: Logging In

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  # get 'sessions/create'
+  # get 'sessions/destroy'
+  resources :users
   resources :orders
   resources :line_items
   resources :carts
@@ -6,6 +16,7 @@ Rails.application.routes.draw do
   resources :products do
     get :who_bought, on: :member
   end
+
   # For details on the DSL available within this file, see
   # https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_06_105053) do
+ActiveRecord::Schema.define(version: 2023_03_14_145656) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -43,6 +43,13 @@ ActiveRecord::Schema.define(version: 2023_03_06_105053) do
     t.text "description"
     t.string "image_url"
     t.decimal "price", precision: 8, scale: 2
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
